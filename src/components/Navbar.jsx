37eb4fef9b2fbd812cb37c87151e96d45eb15840
joel thomas mathew <joelmathew857@gmail.com>
2023-10-17 21:33:46 +0530

    added icons

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+
+const Navbar = () => {
+  return (
+    <div>
+      <AiOutlineMenu />
+    </div>
+  );
+};
+
+export default Navbar;
