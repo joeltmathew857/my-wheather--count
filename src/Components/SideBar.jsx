0d9fce7f6bb03dc67e7e51858666fe62d5d257e0
joel thomas mathew <joelmathew857@gmail.com>
2023-10-17 22:36:42 +0530

    add-color

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const SideBar = () => {
   return (
-    <div>
-      <h1>hi</h1>
+    <div className="">
+      {/*left side*/}
+      <FontAwesomeIcon
+        icon={faBars}
+        className="cursor-pointer sm:h-12 lg:h-12"
+      />
+      <h1 className="text-2xl sm:text-3xl lg:text-4xl ">
+        Best <span className="font-bold">Eats</span>
+      </h1>
     </div>
   );
 };
